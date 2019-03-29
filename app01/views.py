65dd89ea2b9defcb23ff7dc10c6ba82ae3afe82b
weidaijie <951687336@qq.com>
2019-03-29 23:07:05 +0800

    新开发

--- app01/views.py
+++ app01/views.py
@@ -8,5 +8,6 @@ from django.shortcuts import  render,redirect
 def redistest(request):
     t = datetime.datetime.now()
 	print('hahahah')
+	print（'我写了一个程序'）
 
     return render(request,'redis.html',{'t':t})
